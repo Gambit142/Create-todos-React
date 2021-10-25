e622a851fd6a7a8aaae5578c11ef624885109f4a
Gambit142 <nzefrancismaryeugorji@gmail.com>
2021-10-26 00:15:42 +0100

    Store and retrieve the list of todos from the local storage using React life-cycle methods

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -17,6 +17,10 @@ class TodoItem extends React.Component {
       this.setState({ editing: false })
     }
   }
+
+  componentWillUnmount() {
+    console.log("Cleaning up...")
+  }
   
   render() {
     let viewMode = {}
